91d90ba7744c9f8862c19cf4068dd231dc6a61a2
MoJie <mojie@z-srm.com>
2025-02-19 09:57:03 +0800

    fix(designer): 设计器操作面板优化
    1.将点击面板追加普通任务替换为追加用户任务
    2.汉化修改

--- src/modules/ContextPad/index.ts
+++ src/modules/ContextPad/index.ts
@@ -16,10 +16,10 @@ type Shape = import('bpmn-js/lib/model/Types').Shape
 
 class EasyBpmnContextPadProvider extends ContextPadProvider {
     private elementFactory: ElementFactory;
-    private autoPlace: any;
+    private readonly autoPlace: any;
     private create: Create;
     private appendPreview: AppendPreview;
-    private translate: Translate;
+    private readonly translate: Translate;
     constructor(config: ContextPadConfig, injector: Injector, eventBus: EventBus,
                 contextPad: ContextPad, modeling: Modeling, elementFactory: ElementFactory,
                 connect: Connect, create: Create, popupMenu: PopupMenu, canvas: Canvas,
@@ -64,10 +64,10 @@ class EasyBpmnContextPadProvider extends ContextPadProvider {
             }
         } : null;
         // 添加创建用户任务按钮
-        actions['append.append-user-task'] = {
+        actions['append.append-task'] = {
             group: 'model',
             className: 'bpmn-icon-user-task',
-            title: this.translate('User task'),
+            title: this.translate('Append user task'),
             action: {
                 dragstart: appendUserTask,
                 click: append,
