813641830971e4232d3fdda3fc321a45e23c3749
阿杰 <ajie20999@163.com>
2025-07-11 15:05:22 +0800

    feat: npm 发布配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import {defineConfig} from "vite";
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, './src/index.ts'),
+            name: 'solon-flow-bpmn-designer',
+            fileName: `index`,
+            formats: ['es', 'cjs'],
+        },
+    },
+    plugins: [dts()],
     server: {
-        port: 3008
+        port: 3008,
     }
 });
