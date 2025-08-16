12064579ee530347304e7ba8dc4823eb9e0f0cd7
阿杰 <ajie20999@163.com>
2025-08-16 12:27:29 +0800

    feat(flow-vue3): vue3集成solon-flow-bpmn-designer设计器

---
+++ demo/vue3/src/components/SolonFlowDesigner.vue
@@ -0,0 +1,68 @@
+<template>
+    <div ref="designer"></div>
+</template>
+<script setup lang="ts">
+import { onMounted, ref, onUnmounted, watch } from "vue";
+import "solon-flow-bpmn-designer/style.css";
+import { SolonFlowBpmnDesigner } from "solon-flow-bpmn-designer";
+
+interface SolonFlowDesignerProps {
+    value: string;
+    theme?: "light" | "dark";
+    lang?: "en" | "zh";
+    type?: "yaml" | "json";
+    height?: number;
+}
+
+interface SolonFlowDesignerRef {
+    /** 属性面板显隐控制 */
+    panelShow: () => boolean;
+    /** 清除设计器 */
+    clear: () => void;
+    /** 获取值 */
+    getValue: () => string | undefined;
+}
+
+const props = withDefaults(defineProps<SolonFlowDesignerProps>(), {
+    theme: "light",
+    lang: "zh",
+    type: "yaml",
+    height: 60,
+});
+
+watch(() => props.theme, (newTheme) => {
+    solonDesigner.value?.changeTheme(newTheme);
+});
+watch(() => props.lang, (newLang) => {
+    solonDesigner.value?.changeLocal(newLang);
+});
+
+const designer = ref();
+const solonDesigner = ref<SolonFlowBpmnDesigner>();
+const emit = defineEmits(["change"]);
+
+onMounted(() => {
+    solonDesigner.value = new SolonFlowBpmnDesigner({
+        container: designer.value,
+        height: props.height || 60,
+        lang: props.lang || 'zh',
+        theme: props.theme || 'light',
+        value: props.value,
+        onChange: (callback) => {
+            emit("change", callback(props.type))
+        },
+    });
+});
+
+onUnmounted(() => solonDesigner.value?.destroy())
+
+/**
+ * ref可调用方法
+ */
+defineExpose<SolonFlowDesignerRef>({
+    panelShow: () => solonDesigner.value?.showPanel() || false,
+    clear: () => solonDesigner.value?.clear(),
+    getValue: () => solonDesigner.value?.getValue(),
+});
+
+</script>
