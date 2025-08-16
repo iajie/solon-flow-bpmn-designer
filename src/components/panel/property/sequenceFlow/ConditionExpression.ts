11b7862cbccd0e6bbd1c2dde371019661421b0a0
阿杰 <ajie20999@163.com>
2025-08-16 11:29:09 +0800

    feat(flow): 条件表达式修改为高亮

--- src/components/panel/property/sequenceFlow/ConditionExpression.ts
+++ src/components/panel/property/sequenceFlow/ConditionExpression.ts
@@ -1,33 +1,58 @@
 import { PanelInput } from "../PanelInput.ts";
 import { updateCondition } from "../../../../utils/bpmnUtils.ts";
-import { Modeler, Element } from "bpmn-js";
-import { AreaEditor } from "../../../../utils/areaEditor.ts";
+import { Element } from "bpmn-js";
+import { EasyBpmnDialog } from "../../../EasyBpmnDialog.ts";
+import { t } from "i18next";
+import { CodeHighlight } from "../../../CodeHighlight.ts";
 
 export class ConditionExpression extends PanelInput {
+
+    private value: string;
 
     constructor() {
         super();
         this.inputLabel = 'conditionExpression';
+        this.value = '';
         this.init();
     }
 
     init() {
-        this.inputElement = document.createElement('textarea');
-        this.inputElement.style.minHeight = '100px';
-        new AreaEditor(this.inputElement);
+        this.customElement = document.createElement('div');
+        this.customElement.classList.add('property-item-code');
+        this.customElement.style.minHeight = '100px';
+        this.customElement.style.maxHeight = '180px';
+        this.customElement.addEventListener('click', () => {
+            const dialog = new EasyBpmnDialog({
+                edit: true,
+                title: t('whenTitle'),
+                content: this.value || ``,
+            });
+            dialog.addEventListener('code-edit', (e: any) => {
+                updateCondition('expression', e.detail, this.element, this.modeler);
+                this.value = e.detail;
+                this.codeShow();
+            })
+        });
     }
 
     onChange(element: Element) {
         this.onShow(element.businessObject.hasOwnProperty('conditionExpression') && element.businessObject.conditionExpression !== null);
-        this.inputElement && (this.inputElement.value = element.businessObject.conditionExpression?.body || "");
+        this.value = element.businessObject.conditionExpression?.body || '';
+        this.codeShow();
     }
 
     updateElement(element: Element) {
         this.onShow(element.businessObject.hasOwnProperty('conditionExpression') && element.businessObject.conditionExpression !== null);
     }
 
-    onChangeValue(e: Event, element: Element, modeler?: Modeler) {
-        const value = (e.target as HTMLInputElement).value;
-        updateCondition('expression', value, element, modeler);
+    codeShow() {
+        if (this.customElement.children.length) {
+            for (let i = 0; i < this.customElement.children.length; i++) {
+                this.customElement.removeChild(this.customElement.children[i]);
+            }
+        }
+        this.customElement.appendChild(new CodeHighlight({
+            source: this.value,
+        }));
     }
 }
